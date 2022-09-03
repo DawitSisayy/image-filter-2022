d14d2106cc4874cbc9c3dc87f31ef6864439957f
DawitSisayy <dawitsisay42@gmail.com>
2022-09-03 10:53:01 +0300

    we updated th server.ts file

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,9 @@ import {Router, Request, Response} from 'express';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 import fs from 'fs';
 import Jimp from 'jimp';
+import axios, { AxiosRequestConfig, AxiosPromise } from 'axios';
 
-
-
+const axios = require('axios').default;
 (async () => {
 
   // Init the Express application
@@ -56,6 +56,20 @@ import Jimp from 'jimp';
 
     
   });
+
+  /*
+  axios({
+    method: 'get',
+    url: failingImageUrl,
+    responseType: 'arraybuffer'
+    })
+    .then(function ({data: imageBuffer}) {
+     return jimp.read(imageBuffer)
+    })
+*/
+
+
+
 
 
   //! END @TODO1
